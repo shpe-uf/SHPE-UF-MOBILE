481bdbdfd5abdb6bbeb1ead67e28dc8c2b812a9a
isamitre <isamitre@gmail.com>
2020-10-24 20:10:04 -0400

    put everything in Points

--- App.js
+++ App.js
@@ -7,30 +7,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 import gql from "graphql-tag";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 
-import PointsBar from "./components/PointsBar";
+import Points from "./Pages/Points";
 
 export default function App() {
 
-/*  var {
-    user: { id, username }
-  } = useContext(AuthContext);
-  */
-/*  var id = "5f90e4d4920bab09f6df0106"; */
-
-  var {data, refetch} = useQuery(FETCH_USER_QUERY, {
-    variables: {
-      userId: "5f90e4d4920bab09f6df0106"
-    }
-  });
-
-  if(data){
-    var user = data.getUser;
-  }
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <PointsBar/>
+      <Points/>
     </View>
   );
 }
@@ -43,31 +28,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-const FETCH_USER_QUERY = gql`
-  query getUser($userId: ID!) {
-    getUser(userId: $userId) {
-      firstName
-      lastName
-      points
-      fallPoints
-      springPoints
-      summerPoints
-      fallPercentile
-      springPercentile
-      summerPercentile
-      events {
-        name
-        category
-        createdAt
-        points
-      }
-      tasks {
-        name
-        points
-        startDate
-      }
-      bookmarkedTasks
-    }
-  }
-`;
