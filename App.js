86cabab677d865a7b18926503701147a5c8419e8
MarianaTorresTorres <mari.torret@gmail.com>
2020-10-24 23:37:27 -0400

    Just getting everything set up

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
+import Button from "./Pages/Button";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button />
     </View>
   );
 }
@@ -17,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ Pages/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Button() {
+  return <Text>MY SHITS WORKS lol</Text>;
+}
+
+export default Button;
