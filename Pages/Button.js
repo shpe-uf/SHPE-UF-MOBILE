c41805b96e4f5263e0fbcdd7ca17a8af3244f976
MarianaTorresTorres <mari.torret@gmail.com>
2020-11-02 12:03:30 -0500

    made a button

--- Pages/Button.js
+++ Pages/Button.js
@@ -2,11 +2,7 @@ import React from "react";
 import { View, Text } from "react-native";
 
 function Button() {
-  return (
-    <Button
-        title='Add code'
-    />
-  )
+  return <Button title="Add code" />;
 }
 
 export default Button;
