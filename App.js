c36989c28ac9e66f10e59dadfb59ad0f0468bf18
isamitre <isamitre@gmail.com>
2020-11-02 20:58:58 -0500

    points are formatted right with percentiles

--- App.js
+++ App.js
@@ -16,6 +16,10 @@ let user = {
   "fallPoints": 2,
   "springPoints": 1,
   "summerPoints": 0,
+  "fallPercentile": '99%',
+  "springPercentile": '99%',
+  "summerPercentile": '99%',
+
 }
 
   return (
--- Pages/Points.js
+++ Pages/Points.js
@@ -1,14 +1,57 @@
 import React, { useContext, useState } from "react";
-import { View, Text } from "react-native";
+import { Alert, Button, View, Text, StyleSheet } from "react-native";
 
 const Points = ({ user }) => {
   return (
-    <View>
-      <Text>Fall Points: {user ? user.fallPoints : "0"}</Text>
-      <Text>Spring Points: {user ? user.springPoints : "0"}</Text>
-      <Text>Summer Points: {user ? user.summerPoints : "0"}</Text>
+    <View style={styles.container}>
+    <Button
+      style={styles.button}
+      title="Redeem Code"
+      onPress={() => Alert.alert('Redeem Code button pressed.')}
+      />
+    <View style={styles.pointsBar}>
+      <View style={styles.pointsBox}>
+        <Text>FALL POINTS</Text>
+        <Text>{user ? user.fallPoints : "0"}</Text>
+        <Text>{user ? user.fallPercentile : "0"} percentile</Text>
+      </View>
+      <View style={styles.pointsBox}>
+        <Text>SPRING POINTS</Text>
+        <Text>{user ? user.springPoints : "0"}</Text>
+        <Text>{user ? user.springPercentile : "0"} percentile</Text>
+      </View>
+      <View style={styles.pointsBox}>
+        <Text>SUMMER POINTS</Text>
+        <Text>{user ? user.summerPoints : "0"}</Text>
+        <Text>{user ? user.summerPercentile : "0"}</Text>
+      </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    width: '100%',
+  },
+  button: {
+    width: '30%',
+  },
+  pointsBar: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  pointsBox: {
+    backgroundColor: 'powderblue',
+    borderColor: 'gray',
+    textAlign: 'center',
+    width: '80%',
+    padding: '10px',
+    margin: '10px',
+  },
+});
 
 export default Points;
