1c590651992dc8b4f97b1ed7afc0609c988bf832
MarianaTorresTorres <mari.torret@gmail.com>
2020-11-09 04:27:20 -0500

    user can input code

--- Pages/CodeButton.js
+++ Pages/CodeButton.js
@@ -1,5 +1,6 @@
-import * as React from "react";
-import { Text, View, StyleSheet, TextInput, Button, Alert } from "react-native";
+//import * as React from "react";
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput, Button, Alert, Modal, TouchableHighlight } from "react-native";
 import { useForm, Controller } from "react-hook-form";
 import Constants from "expo-constants";
 
@@ -24,44 +25,65 @@ function CodeButton() {
 
   console.log(errors);
 
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Enter code:</Text>
-      <Controller
-        control={control}
-        render={({ onChange, onBlur, value }) => (
-          <TextInput
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={(value) => onChange(value)}
-            value={value}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <Text style={styles.label}>Enter code:</Text>
+        <Controller
+          control={control}
+          render={({ onChange, onBlur, value }) => (
+            <TextInput
+              style={styles.input}
+              onBlur={onBlur}
+              onChangeText={(value) => onChange(value)}
+              value={value}
+            />
+          )}
+          name="code"
+          rules={{ required: true }}
+        />
+
+        <View style={styles.button}>
+          <Button
+            style={styles.buttonInner}
+            color
+            title="Submit"
+            onPress={handleSubmit(onSubmit)}
           />
-        )}
-        name="code"
-        rules={{ required: true }}
-      />
+        </View>
 
-      <View style={styles.button}>
-        <Button
-          style={styles.buttonInner}
-          color
-          title="Submit"
-          onPress={handleSubmit(onSubmit)}
-        />
-      </View>
+        <View style={styles.button}>
+          <Button
+            style={styles.buttonInner}
+            color
+            title="Cancel"
+            onPress={() => {
+              setModalVisible(!modalVisible);
+              reset({
+                code: "",
+              });
+            }}
+          />
+        </View>
+      </Modal>
 
-      <View style={styles.button}>
-        <Button
-          style={styles.buttonInner}
-          color
-          title="Cancel"
-          onPress={() => {
-            reset({
-              code: "",
-            });
-          }}
-        />
-      </View>
+      <TouchableHighlight
+        style={styles.button}
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      >
+        <Text style={styles.textStyle}>Redeem Code</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -71,6 +93,21 @@ const styles = StyleSheet.create({
     color: "white",
     margin: 20,
     marginLeft: 0,
+  },
+  modalView: {
+  margin: 20,
+  backgroundColor: "white",
+  borderRadius: 20,
+  padding: 35,
+  alignItems: "center",
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 2
+  },
+  shadowOpacity: 0.25,
+  shadowRadius: 3.84,
+  elevation: 5
   },
   button: {
     marginTop: 40,
@@ -91,6 +128,12 @@ const styles = StyleSheet.create({
     height: 40,
     padding: 10,
     borderRadius: 4,
+  },
+  textStyle: {
+    padding: 8,
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
   },
 });
 
