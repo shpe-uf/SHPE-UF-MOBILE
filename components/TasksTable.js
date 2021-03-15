82ea1e9802e21e3af6904f10b5b2b735e2fad09f
Isa <isamitre@gmail.com>
2021-03-15 17:34:18 -0400

    pagination works

--- components/TasksTable.js
+++ components/TasksTable.js
@@ -5,19 +5,19 @@ import { DataTable } from "react-native-paper";
 import { useQuery, gql } from "@apollo/client";
 import { FETCH_TASKS_QUERY } from ".././util/graphql";
 
-import Pagination from "./Pagination";
-
-
 function TasksTable() {
   let { data, loading } = useQuery(FETCH_TASKS_QUERY);
 
   let tasks = null;
+  let tablePage = [];
   let tableContents = [];
+  let total = 0;
 
-  const itemsPerPage = 5;
   const [page, setPage] = React.useState(0);
+  const itemsPerPage = 5;
   const from = page * itemsPerPage;
   const to = (page + 1) * itemsPerPage;
+
 
   const monthOptions = require("./../assets/options/month.json");
   const now = new Date();
@@ -27,6 +27,7 @@ function TasksTable() {
   if (data) {
     tasks = data.getTasks;
 
+    const maxTasks = Math.min(tasks.length, 5);
     for (let i = 0; i < tasks.length; i++) {
       const task = tasks[i];
       if (now < Date.parse(task.endDate)) {
@@ -37,7 +38,12 @@ function TasksTable() {
             <DataTable.Cell numeric>{task.points}</DataTable.Cell>
           </DataTable.Row>
         ];
-        tableContents.push(row);
+        tablePage.push(row);
+        if (tablePage.length >= maxTasks || i === tasks.length - 1) {
+          tableContents.push(tablePage);
+          total += tablePage.length;
+          tablePage = [];
+        }
       }
     }
   }
@@ -57,22 +63,13 @@ function TasksTable() {
               <DataTable.Title>End Date</DataTable.Title>
               <DataTable.Title numeric>Points</DataTable.Title>
             </DataTable.Header>
-            {tableContents}
-            {tableContents.length > itemsPerPage ? (
-              <Pagination items={tableContents} />
-              /*
-              <View>
-                <DataTable.Pagination
-                  page={page}
-                  numberOfPages={Math.floor(tableContents.length / itemsPerPage)}
-                  onPageChange={page => setPage(page)}
-                  label={`${from + 1}-${to} of ${tableContents.length}`}
-                />
-              </View>
-              */
-            ) : (
-              <View></View>
-            )}
+            {tableContents[page]}
+            <DataTable.Pagination
+              page={page}
+              numberOfPages={tasks.length}
+              onPageChange={page => setPage(page)}
+              label={`${from + 1}-${Math.min(to,total)} of ${total}`}
+            />
           </DataTable>
         </View>
       )}
