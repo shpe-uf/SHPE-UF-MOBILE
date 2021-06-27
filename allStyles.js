0f52ef1545b379e403f684c6ee2593806db1bc4e
Isa <isamitre@gmail.com>
2021-06-27 14:41:53 -0400

    made stylesheet for project

---
+++ allStyles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%"
+  },
+  page: {
+    alignItems: "center",
+    alignSelf: "center",
+    width: "80%"
+  },
+  content: {
+    width: "100%"
+  },
+  h1: {
+    fontSize: 23,
+    marginBottom: 6,
+    marginTop: 10,
+    color: "#FD652F",
+    fontWeight: "bold",
+    fontStyle: "italic"
+  },
+  h2: {
+    fontSize: 20,
+    color: "#FD652F",
+    marginBottom: 6,
+    fontWeight: "bold",
+    fontStyle: "italic"
+  }
+});
